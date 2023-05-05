5d668cfc9526f774f73e98d8fde6017175c1e168
Dimitris Pantazis <pantazisd@outlook.com>
2023-05-05 17:32:05 +0300

    Remove provisioners

--- main.tf
+++ main.tf
@@ -32,15 +32,6 @@ resource "azurerm_container_registry" "acr" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Basic"
-
-  provisioner "local-exec" {
-    command = "az acr login -n ${var.acr_name}"
-  }
-
-  provisioner "local-exec" {
-    command = "docker push ${var.acr_name}.azurecr.io/tools/azurenamingtool"
-  }
-
 }
 
 resource "azurerm_role_assignment" "role_assignment" {
