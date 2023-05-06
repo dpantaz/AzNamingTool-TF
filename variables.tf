5212f1ba2bd23abfbac399890620bd48faf3ccba
Dimitris Pantazis <pantazisd@outlook.com>
2023-05-06 09:36:58 +0300

    Add variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,35 +1,35 @@
 variable "location" {
-  description = "value"
+  description = "The Azure region for the deployment"
   type        = string
 }
 
 variable "resource_group_name" {
-  description = "value"
+  description = "The name of the Azure Resource Group"
   type        = string
 }
 
 variable "storage_account_name" {
-  description = "value"
+  description = "The name of the Azure Storage Account"
   type        = string
 }
 
 variable "managed_identity_name" {
-  description = "value"
+  description = "The name of the User Assigned Managed Identity"
   type        = string
 }
 
 variable "app_service_plan_name" {
-  description = "value"
+  description = "The name of the App Service Plan"
   type        = string
 }
 
 variable "app_service_name" {
-  description = "value"
+  description = "The name of the Web App"
   type        = string
 }
 
 variable "acr_name" {
-  description = "value"
+  description = "The name of the Azure Container Registry"
   type        = string
 
 }
